cc3a220c01c0db16df80d73700f6aec59e875383
spinales <saul021002@gmail.com>
2024-01-07 02:11:02 -0400

    update setup script

--- setup.sh
+++ setup.sh
@@ -2,8 +2,7 @@
 
 # install
 sudo apt update
-sudo apt install -y nginx postgresql postgresql-contrib golang
-
+sudo apt install -y nginx postgresql postgresql-contrib golang make
 
 # postgres
 sudo systemctl start postgresql.service
@@ -13,12 +12,12 @@ sudo -u postgres createdb northwind
 psql -U postgres -d northwind -f ./northwind.sql
 
 # app
-printf "export PATH=\$PATH:/usr/local/go/bin" >> ~/.profile
+sudo sh -c 'printf "export PATH=\$PATH:/usr/local/go/bin" >> ~/.profile'
 go version
 sudo touch /etc/systemd/system/app.service
 sudo chmod 664 /etc/systemd/system/app.service
 make compile
-printf "[Unit]\nDescription=Construccion Demo\n\n[Service]\nExecStart=/opt/demo/build/demo\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/app.service
+sudo sh -c 'printf "[Unit]\nDescription=Construccion Demo\n\n[Service]\nExecStart=/opt/demo/build/demo\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/app.service'
 sudo systemctl enable app
 sudo systemctl start app
 sudo systemctl status app
@@ -29,6 +28,6 @@ sudo systemctl start nginx
 sudo ufw allow 'Nginx HTTP'
 sudo ufw status
 systemctl status nginx
-printf "server {\n\tlisten 80;\n\tlisten 443 ssl;\n\n\tlocation / {\n\t\tproxy_pass http://localhost:8080;\n\t\tproxy_set_header Host \$host;\n\t\tproxy_set_header X-Real-IP \$remote_addr;\n\t\tproxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;\n\t\tproxy_set_header X-Forwarded-Proto \$scheme;\n\t}\n\n}" > /etc/nginx/conf.d/default.conf
+sudo sh -c 'printf "server {\n\tlisten 80;\n\tlisten 443 ssl;\n\n\tlocation / {\n\t\tproxy_pass http://localhost:8080;\n\t\tproxy_set_header Host \$host;\n\t\tproxy_set_header X-Real-IP \$remote_addr;\n\t\tproxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;\n\t\tproxy_set_header X-Forwarded-Proto \$scheme;\n\t}\n\n}" > /etc/nginx/conf.d/default.conf'
 nginx -t
 nginx -s reload
